2da75a56d537e6cc756f9bb6b270634a70126935
ShunHW <77417338+ShunHW@users.noreply.github.com>
2021-02-15 19:34:05 -0500

    Connect to database

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,32 +1,38 @@
-import psycopg2
 from flask import Flask
+from sshtunnel import SSHTunnelForwarder
+import psycopg2
 
 app = Flask(__name__)
-#conn = psycopg2.connect(
-#   host="51.222.151.27",
-#    database="postgres",
-#    user="root",
-#    password="COSC4P02Raters")
 
-@app.route('/', methods =['GET'])
-def default():
-    #code goes here
+server = SSHTunnelForwarder(
+    ('51.222.151.27',22),
+    ssh_private_key = "C:\\Users\\wsh41\\.ssh\\id_rsa",
+    ssh_username='root',
+    remote_bind_address=('localhost',5432))
+
+server.start()
+print("server connected")
+
+conn = psycopg2.connect(
+    database='postgres',
+    user='postgres',
+    password='COSC4P02Raters',
+    host='localhost',
+    port=server.local_bind_port,
+)
+curs = conn.cursor()
+print("database connected")
 
 
-    return 'HEY EVERYONE!'
 
 
-@app.route('/mainpage', methods=['GET'])
-def mainPage():
-    # code goes here
+@app.route('/')
+def hello_world():
+    curs.execute("select version()")
+    data = curs.fetchone()
+    print(data)
+    return "%s" % data
 
-    return 'ALL THE INFO WE NEED FOR THE MAIN PAGE'
-
-@app.route('/SchoolInfo', methods=['GET'])
-def SchoolInfo():
-    # code goes here
-
-    return 'ALL THE INFO WE NEED FOR THE School\'s Page'
 
 if __name__ == '__main__':
     app.run()
