96058347ae99b4b042d6e2f58b3b53b513fb62c4
Tianci Yang <tommyjarvisjyn@gmail.com>
2021-02-21 00:56:38 -0500

    Change folder name. components -> institutions

--- material-angular-dashboard/src/app/app-routing.module.ts
+++ material-angular-dashboard/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
-import { LayoutsModule } from './layouts';
-import { CommonLayoutComponent } from './layouts/common-layout';
-import { ChartsComponent } from './pages/charts';
-import { ComponentsComponent } from './pages/components';
-import { InstitutionDetailsComponent } from './pages/institutionDetails';
-import { DashboardComponent } from './pages/dashboard';
-import { Dashboard2Component } from './pages/dashboard2';
+import {LayoutsModule} from './layouts';
+import {CommonLayoutComponent} from './layouts/common-layout';
+import {ChartsComponent} from './pages/charts';
+import {ComponentsComponent} from './pages/institutions';
+import {InstitutionDetailsComponent} from './pages/institutionDetails';
+import {DashboardComponent} from './pages/dashboard';
+import {Dashboard2Component} from './pages/dashboard2';
 
 // @ts-ignore
 // @ts-ignore
@@ -16,24 +16,27 @@ import { Dashboard2Component } from './pages/dashboard2';
   imports: [
     RouterModule.forRoot(
       [
-        { path: '', redirectTo: 'app/home', pathMatch: 'full' },
-        { path: 'app', component: CommonLayoutComponent, children: [
-          { path: 'home', component: DashboardComponent, pathMatch: 'full' },
-          { path: 'professors', component: Dashboard2Component, pathMatch: 'full' },
-          { path: 'departments', component: ChartsComponent, pathMatch: 'full'},
-          { path: 'courses', component: ChartsComponent, pathMatch: 'full'},
-          { path: 'institutions', component: ComponentsComponent, pathMatch: 'full' },
-          { path: 'institutionDetails', component: InstitutionDetailsComponent, pathMatch: 'full' },
-          { path: 'about', component: ComponentsComponent, pathMatch: 'full' },
-          { path: '**', redirectTo: '/ypages/404' },
-        ] }, // add 'canActivate: AuthGuard' for catching unauth users
-        { path: 'pages', loadChildren: () => import('./pages/pages/pages.module').then(m => m.PagesModule) }, //This allows requests for visiting child pages under this path.(short url + auto mapping)
-        { path: '**', redirectTo: '/pages/404' },
+        {path: '', redirectTo: 'app/home', pathMatch: 'full'},
+        {
+          path: 'app', component: CommonLayoutComponent, children: [
+            {path: 'home', component: DashboardComponent, pathMatch: 'full'},
+            {path: 'professors', component: Dashboard2Component, pathMatch: 'full'},
+            {path: 'departments', component: ChartsComponent, pathMatch: 'full'},
+            {path: 'courses', component: ChartsComponent, pathMatch: 'full'},
+            {path: 'institutions', component: ComponentsComponent, pathMatch: 'full'},
+            {path: 'institutionDetails', component: InstitutionDetailsComponent, pathMatch: 'full'},
+            {path: 'about', component: ComponentsComponent, pathMatch: 'full'},
+            {path: '**', redirectTo: '/ypages/404'},
+          ]
+        }, // add 'canActivate: AuthGuard' for catching unauth users
+        {path: 'pages', loadChildren: () => import('./pages/pages/pages.module').then(m => m.PagesModule)}, //This allows requests for visiting child pages under this path.(short url + auto mapping)
+        {path: '**', redirectTo: '/pages/404'},
       ],
-      { useHash: true },
+      {useHash: true},
     ),
     LayoutsModule,
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- material-angular-dashboard/src/app/app.module.ts
+++ material-angular-dashboard/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AuthInterceptor, AuthService, FakeBackendInterceptor } from '@services/
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartsModule } from './pages/charts';
-import { ComponentsModule } from './pages/components';
+import { ComponentsModule } from './pages/institutions';
 import { InstitutionDetailsModule } from './pages/institutionDetails';
 
 import { DashboardModule } from './pages/dashboard';
