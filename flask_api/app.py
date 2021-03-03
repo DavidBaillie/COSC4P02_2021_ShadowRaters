cb6239bc92f2ab7b7378156da1e15b59256badcd
ShunHW <77417338+ShunHW@users.noreply.github.com>
2021-03-03 13:30:23 -0500

    blue print

--- flask_api/app.py
+++ flask_api/app.py
@@ -7,7 +7,9 @@ import psycopg2
 import os,binascii
 from hashlib import sha256
 import random, string
+from views.test import test
 app = Flask(__name__)
+app.register_blueprint(test,url_prefix='/test')
 app.secret_key = "adaslccadw"
 auth = HTTPBasicAuth()
 SECRET_KEY = "qbsacasd"
@@ -48,30 +50,30 @@ print("database connected")
 def hello_world():
     return "hello world"
 
-@app.route("/sign_up",methods=["POST"])
-def createNewUser():
-    newUser = request.get_json()
-    uuid = binascii.b2a_hex(os.urandom(15))
-    uuid = str(uuid,encoding="utf-8")
-    admin = newUser.get("admin")
-    username = newUser.get("username")
-    email = newUser.get("email")
-    password = newUser.get("password")
-    salt = ''.join(random.sample(string.ascii_letters + string.digits,10))
-    password = sha256(salt.encode()+password.encode()).hexdigest()
-    school = newUser.get("school")
-    program = newUser.get("program")
-    if not all([username,password]):
-        return jsonify(msg="missing admin, username or password")
-    curs.execute("select * from \"user\" where username = '%s'" % username)
-    data = curs.fetchone()
-    #print(data)
-    if(data is not None):
-        return jsonify(msg="username already exist")
-    curs.execute("insert into \"user\" "
-                 "values('%s','%s','%s','%s','%s','%s','%s','%s')" % (uuid,admin,username,email,password,salt,school,program))
-    conn.commit()
-    return jsonify(msg="sign up success")
+# @app.route("/sign_up",methods=["POST"])
+# def createNewUser():
+#     newUser = request.get_json()
+#     uuid = binascii.b2a_hex(os.urandom(15))
+#     uuid = str(uuid,encoding="utf-8")
+#     admin = newUser.get("admin")
+#     username = newUser.get("username")
+#     email = newUser.get("email")
+#     password = newUser.get("password")
+#     salt = ''.join(random.sample(string.ascii_letters + string.digits,10))
+#     password = sha256(salt.encode()+password.encode()).hexdigest()
+#     school = newUser.get("school")
+#     program = newUser.get("program")
+#     if not all([username,password]):
+#         return jsonify(msg="missing admin, username or password")
+#     curs.execute("select * from \"user\" where username = '%s'" % username)
+#     data = curs.fetchone()
+#     #print(data)
+#     if(data is not None):
+#         return jsonify(msg="username already exist")
+#     curs.execute("insert into \"user\" "
+#                  "values('%s','%s','%s','%s','%s','%s','%s','%s')" % (uuid,admin,username,email,password,salt,school,program))
+#     conn.commit()
+#     return jsonify(msg="sign up success")
 
 @app.route("/sign_in",methods=["POST"])
 def sign_in():
