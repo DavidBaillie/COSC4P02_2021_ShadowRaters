e4925b3a834411ac874fcaf34fc03739e40219f9
RodoVJ <rodovj29@icloud.com>
2021-03-14 23:51:54 -0400

    added func to rate page

--- material-angular-dashboard/src/app/pages/institutionDetails/institutionDetails.component.ts
+++ material-angular-dashboard/src/app/pages/institutionDetails/institutionDetails.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { UpgradableComponent } from 'theme/components/upgradable';
 
 @Component({
@@ -6,9 +6,13 @@ import { UpgradableComponent } from 'theme/components/upgradable';
   templateUrl: './institutionDetails.component.html',
   styleUrls: ['./institutionDetails.component.scss'],
 })
-export class InstitutionDetailsComponent extends UpgradableComponent {
+export class InstitutionDetailsComponent extends UpgradableComponent implements OnInit {
   @HostBinding('class.mdl-grid') public readonly mdlGrid = true;
   @HostBinding('class.ui-components') public readonly uiComponents = true;
+
+  data_comments:Array<Object>;
+  modal:HTMLElement;
+  modalTextArea:HTMLTextAreaElement;
 
   public data = [
     {
@@ -17,23 +21,65 @@ export class InstitutionDetailsComponent extends UpgradableComponent {
     },
   ];
 
+  ngOnInit() {
+    this.data_comments = [
+      {
+        userID:'Mathieu Cote',
+        comment:'I think we did a Pretty Good job so far!',
+        rating:'4.5',
+      },
+      {
+        userID:'Spongebob',
+        comment:'Always follow your heart – unless your heart is bad with directions!',
+        rating:'5.0',
+      },
+      {
+        userID:'Squidward',
+        comment:"It would be if I didn't have to go to work.",
+        rating:'3.8',
+      },
+      {
+        userID:'Mathieu Cote',
+        comment:'I think we did a Pretty Good job so far!',
+        rating:'4.5',
+      },
+      {
+        userID:'Spongebob',
+        comment:'Always follow your heart – unless your heart is bad with directions!',
+        rating:'5.0',
+      },
+      {
+        userID:'Squidward',
+        comment:"It would be if I didn't have to go to work.",
+        rating:'3.8',
+      },
+    ];
 
-  public data_comments = [
-    {
-      userID:'Mathieu Cote',
-      comment:'I think we did a Pretty Good job so far!',
-      rating:'4.5',
-    },
-    {
-      userID:'Spongebob',
-      comment:'Always follow your heart – unless your heart is bad with directions!',
-      rating:'5.0',
-    },
-    {
-      userID:'Squidward',
-      comment:"It would be if I didn't have to go to work.",
-      rating:'3.8',
-    },
-  ];
+    this.modal = document.getElementById("myModal");
+    this.modalTextArea = <HTMLTextAreaElement> document.getElementById("modalTextArea");
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = (event) => {
+      if (event.target == this.modal) {
+        this.modal.style.display = "none";
+      }
+    }
+  }
+
+  public openModal():void {
+    this.modal.style.display = "block";
+  }  
+
+  public submitRating():void {
+    this.data_comments.unshift(
+      {
+        date: "Jan 10th, 2021",
+        comment: this.modalTextArea.value,
+      }
+    );
+    this.modalTextArea.value = " ";
+    this.modal.style.display = "none";
+  }
+   
 
 }
--- material-angular-dashboard/src/app/pages/institutionDetails/institutionDetails.module.ts
+++ material-angular-dashboard/src/app/pages/institutionDetails/institutionDetails.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {InstitutionDetailsComponent} from './institutionDetails.component';
 import {DiscreteBarChartComponent} from "./discrete-bar-chart";
+import {LineChartComponent} from "./line-chart";
 
 import {ThemeModule} from 'theme';
 
@@ -18,6 +19,7 @@ import {ThemeModule} from 'theme';
   declarations: [
     InstitutionDetailsComponent,
     DiscreteBarChartComponent,
+    LineChartComponent,
   ],
 })
 export class InstitutionDetailsModule {
