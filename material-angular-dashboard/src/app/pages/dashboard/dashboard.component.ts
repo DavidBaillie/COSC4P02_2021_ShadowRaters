7431579a0aff10c79d37b026d72cf03ef2aed4c3
Tianci Yang <tommyjarvisjyn@gmail.com>
2021-03-16 20:11:53 -0400

    Added home page routing links to searches.

--- material-angular-dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ material-angular-dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,28 @@
-import { Component, HostBinding } from '@angular/core';
-
-import { UpgradableComponent } from 'theme/components/upgradable';
+import {Component, HostBinding} from '@angular/core';
+import {Router} from '@angular/router';
+import {UpgradableComponent} from 'theme/components/upgradable';
 
 @Component({
   selector: 'app-dashboard',
   styleUrls: ['../departments/departments.component.scss', '../../../theme/scss/centre.scss'],
   templateUrl: './dashboard.component.html',
 })
+
 export class DashboardComponent extends UpgradableComponent {
   @HostBinding('class.mdl-grid') public readonly mdlGrid = true;
   @HostBinding('class.mdl-grid--no-spacing') public readonly mdlGridNoSpacing = true;
+
+  constructor(private router: Router) {
+    super();
+
+  }
+
+  route_search_prof = function () {
+    this.router.navigateByUrl('/app/professors');
+  };
+
+  route_search_institution = function () {
+    this.router.navigateByUrl('/app/institutions');
+  };
 }
+
