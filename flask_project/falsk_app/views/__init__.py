6147e22920c4e5c6346debb872e4e84d89e8541a
ShunHW <77417338+ShunHW@users.noreply.github.com>
2021-03-26 16:21:27 -0400

    try to fix some bug

--- flask_project/falsk_app/views/__init__.py
+++ flask_project/falsk_app/views/__init__.py
@@ -1,21 +1,20 @@
 from flask import Flask
-from sshtunnel import SSHTunnelForwarder
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 
 
 app = Flask(__name__)
-server = SSHTunnelForwarder(
-    ('51.222.151.27',22),
-    ssh_private_key = "C:\\Users\\wsh41\\.ssh\\id_rsa",
-    ssh_username='root',
-    remote_bind_address=('localhost',5432))
-server.start()
-print("server connected")
+# server = SSHTunnelForwarder(
+#     ('51.222.151.27',22),
+#     ssh_private_key = "C:\\Users\\wsh41\\.ssh\\id_rsa",
+#     ssh_username='root',
+#     remote_bind_address=('localhost',5432))
+# server.start()
+# print("server connected")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:123456@localhost:5432/testDB'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:123456@localhost:5432/testDB'
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:COSC4P02Raters@localhost:5432/postgres'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:COSC4P02Raters@localhost:5432/postgres'
 db = SQLAlchemy(app)
 
 Base = automap_base()
