4423d4c0b0add1cc5b8739201ce6d69e71467ea1
RodoVJ <rodovj29@icloud.com>
2021-04-20 21:54:11 -0400

    make search-institution more generic

--- material-angular-dashboard/src/app/pages/search/search-institution/search-institution.module.ts
+++ material-angular-dashboard/src/app/pages/search/search-institution/search-institution.module.ts
@@ -1,11 +1,12 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MaterialAngularSelectModule} from 'material-angular-select';
-import {ThemeModule} from 'theme';
-import {HomePageModule} from '../../homePage/homePage.module';
-import {RouterModule} from '@angular/router';
-import {SearchInstitution} from './search-institution.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialAngularSelectModule } from 'material-angular-select';
+import { ThemeModule } from 'theme';
+import { HomePageModule } from '../../homePage/homePage.module';
+import { RouterModule } from '@angular/router';
+import { SearchInstitution } from './search-institution.component';
+import {SearchModule} from '../search-general'
 
 @NgModule({
   imports: [
@@ -15,10 +16,10 @@ import {SearchInstitution} from './search-institution.component';
     HomePageModule,
     MaterialAngularSelectModule,
     RouterModule,
+    SearchModule,
   ],
   declarations: [
     SearchInstitution,
   ],
 })
-export class SearchInstitutionModule {
-}
+export class SearchInstitutionModule {}
