b9f80e29ccaf0fa01024026b69cc99ae05c3f429
Tianci Yang <tommyjarvisjyn@gmail.com>
2021-04-25 06:06:27 -0400

    -Trying to resolve confirm password issue.

--- material-angular-dashboard/src/app/pages/pages/forgot-password/forgot-password.component.ts
+++ material-angular-dashboard/src/app/pages/pages/forgot-password/forgot-password.component.ts
@@ -7,4 +7,9 @@ import { BlankLayoutCardComponent } from 'app/components/blank-layout-card';
   styleUrls: ['../../../components/blank-layout-card/blank-layout-card.component.scss'],
   templateUrl: './forgot-password.component.html',
 })
-export class ForgotPasswordComponent extends BlankLayoutCardComponent { }
+export class ForgotPasswordComponent extends BlankLayoutCardComponent {
+
+
+
+
+}
